882b0f151343b9eaf8173b976d86554e5118ea55
Andrés Díaz Soto <andres@estudiomanati.com>
2016-06-20 14:42:11 -0600

    RP-14: Move enquire.js to separated library.

--- themes/custom/retopais/template.php
+++ themes/custom/retopais/template.php
@@ -3,3 +3,11 @@
  * @file
  * Reto Pais theme templating functions.
  */
+
+/**
+ * Implements template_preprocess_page().
+ */
+function retopais_preprocess_page(&$variables) {
+  // Add enquire.js.
+  drupal_add_js(libraries_get_path('enquirejs') . '/enquire.min.js');
+}
