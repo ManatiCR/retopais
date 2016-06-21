9781758eacdccdf49c6834494d55020f15b7f0bb
Andrés Díaz Soto <andres@estudiomanati.com>
2016-06-20 18:45:52 -0600

    RP-14: Fix template.php to use correct preprocess function.

--- themes/custom/retopais/template.php
+++ themes/custom/retopais/template.php
@@ -5,9 +5,9 @@
  */
 
 /**
- * Implements template_preprocess_page().
+ * Implements template_preprocess_html().
  */
-function retopais_preprocess_page(&$variables) {
+function retopais_preprocess_html(&$variables) {
   // Add enquire.js.
   drupal_add_js(libraries_get_path('enquirejs') . '/enquire.min.js');
 }
