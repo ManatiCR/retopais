c04f39a11959a8a09ec100f637057f3eb70f6959
Edgar Barrantes <edgarbb@gmail.com>
2016-06-24 12:37:19 -0600

    RP-13: Adds new template for full width pages.

---
+++ themes/custom/retopais/panels-layouts/retopais_fullwidth_areas/retopais_fullwidth_areas.tpl.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Reto Pais Page with Sidebar layout template.
+ */
+?>
+
+<section class="page page-sidebar clearfix">
+
+  <?php if (!empty($content['first-full'])): ?>
+    <div class="page-fullwidth-first-full">
+      <?php
+      print $content['first-full'];
+      ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['second'])): ?>
+    <div class="page-fullwidth-second">
+      <?php
+      print $content['second'];
+      ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['third-full'])): ?>
+    <div class="page-fullwidth-third-full">
+      <?php
+      print $content['third-full'];
+      ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (!empty($content['fourth'])): ?>
+    <div class="page-fullwidth-fourth">
+      <?php
+      print $content['fourth'];
+      ?>
+    </div>
+  <?php endif; ?>
+
+</section>
