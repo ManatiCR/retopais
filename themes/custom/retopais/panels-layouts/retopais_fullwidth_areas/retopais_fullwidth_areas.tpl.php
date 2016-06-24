2e15ec59d2d9b699c1f12452526421b6cc0da7a4
Edgar Barrantes <edgarbb@gmail.com>
2016-06-24 16:57:13 -0600

    RP-13: Syntax changes.

--- themes/custom/retopais/panels-layouts/retopais_fullwidth_areas/retopais_fullwidth_areas.tpl.php
+++ themes/custom/retopais/panels-layouts/retopais_fullwidth_areas/retopais_fullwidth_areas.tpl.php
@@ -9,33 +9,25 @@
 
   <?php if (!empty($content['first-full'])): ?>
     <div class="page-fullwidth-first-full">
-      <?php
-      print $content['first-full'];
-      ?>
+      <?php print $content['first-full']; ?>
     </div>
   <?php endif; ?>
 
   <?php if (!empty($content['second'])): ?>
     <div class="page-fullwidth-second">
-      <?php
-      print $content['second'];
-      ?>
+      <?php print $content['second']; ?>
     </div>
   <?php endif; ?>
 
   <?php if (!empty($content['third-full'])): ?>
     <div class="page-fullwidth-third-full">
-      <?php
-      print $content['third-full'];
-      ?>
+      <?php print $content['third-full']; ?>
     </div>
   <?php endif; ?>
 
   <?php if (!empty($content['fourth'])): ?>
     <div class="page-fullwidth-fourth">
-      <?php
-      print $content['fourth'];
-      ?>
+      <?php print $content['fourth']; ?>
     </div>
   <?php endif; ?>
 
