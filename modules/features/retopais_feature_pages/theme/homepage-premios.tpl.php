c563dace062c4f4b6ce65aaad558d56e5db92afd
Edgar Barrantes <edgarbb@gmail.com>
2016-06-27 01:55:01 -0600

    RP-45: Changes homepage prices info.

--- modules/features/retopais_feature_pages/theme/homepage-premios.tpl.php
+++ modules/features/retopais_feature_pages/theme/homepage-premios.tpl.php
@@ -7,15 +7,8 @@
 
 <div class="homepage-premios--container">
   <h2>Premios</h2>
-  <div class="homepage-premios--ideas">
-    <span class="homepage-premios--type">Ideas</span>
-    <h3 class="homepage-premios--title"><?php print $ideas_title; ?></h3>
-    <div class="homepage-premios--description"><?php print $ideas_description; ?></div>
-  </div>
-  <div class="homepage-premios--solution">
-    <span class="homepage-premios--type">Solución</span>
-    <h3 class="homepage-premios--title"><?php print $solution_title; ?></h3>
-    <div class="homepage-premios--description"><?php print $solution_description; ?></div>
+  <div class="homepage-premios--description">
+    <?php print $description_premio; ?>
   </div>
   <footer class="homepage-premios--cta">
     <?php print $link; ?>
