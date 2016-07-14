ef7792d76fe7215f8b54d6c09eeda0ff6cbf5eb9
Edgar Barrantes <edgarbb@gmail.com>
2016-07-14 10:21:42 -0600

    RP-58: Makes counters of proposals in general, not ideas and solutions.

--- modules/features/retopais_feature_proposals/theme/proposal-counter-hp.tpl.php
+++ modules/features/retopais_feature_proposals/theme/proposal-counter-hp.tpl.php
@@ -7,7 +7,7 @@
 <div class="que-es-reto-pais--container">
   <div class="que-es-reto-pais--container-inner">
     <div class="que-es-reto-pais--left">
-      <h2 class="que-es-reto-pais--title">¡<?php print $cantidad_ideas; ?> ideas y <?php print $cantidad_soluciones; ?> soluciones en marcha han sido enviadas!</h2>
+      <h2 class="que-es-reto-pais--title">¡<?php print $cantidad_propuestas; ?> propuestas han sido enviadas!</h2>
       <p class="que-es-reto-pais--description">
         <?php print $text; ?>
       </p>
